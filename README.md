04bacdcab09e7029dc868509c3e203c7d05a2dfb
cw7 <support@lembed.org>
2016-06-02 20:13:03 +0800

    re upload source code

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stm32f4-discovery-gcc-builder
+Template projects for STM32f4 devices 
---
+++ c++/inc/ltoa.h
@@ -0,0 +1,24 @@
+/*
+**  LTOA.C
+**
+**  Converts a long integer to a string.
+**
+**  Copyright 1988-90 by Robert B. Stout dba MicroFirm
+**
+**  Released to public domain, 1991
+**
+**  Parameters: 1 - number to be converted
+**              2 - buffer in which to build the converted string
+**              3 - number base to use for conversion
+**
+**  Returns:  A character pointer to the converted string if
+**            successful, a NULL pointer if the number base specified
+**            is out of range.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFSIZE (sizeof(long) * 8 + 1)
+
+char *ltoa(long N, char *str, int base);
